90550f70b926469b0695013fdf9f961e2f6172ab
amitca75 <33972813+amitca75@users.noreply.github.com>
2021-09-07 11:59:33 -0400

    Update CloudEventsController.cs

--- cloudevents/cloudevents-dotnet/Controllers/CloudEventsController.cs
+++ cloudevents/cloudevents-dotnet/Controllers/CloudEventsController.cs
@@ -84,7 +84,7 @@ namespace CloudEventsSample.Controllers
                 new CloudEvent(CloudEventResponseType, new Uri(CloudEventResponseUri))
                 {
                     DataContentType = new ContentType(MediaTypeNames.Application.Json),
-                    Data = new SampleOutput {Message = $"Hello there !!!, {input.Name}"},
+                    Data = new SampleOutput {Message = $"Hello there v1 !!!, {input.Name}"},
                 },
                 ContentMode.Structured,
                 new JsonEventFormatter()
